79c93cc653d0a59983f316bbd1e57e24269eee61
lowgravity1203 <matthew.f.barnes1203@gmail.com>
2020-08-25 12:06:25 -0700

    adds mainheader with styles and empty nav bar

--- crunch-frontend/src/App.js
+++ crunch-frontend/src/App.js
@@ -3,18 +3,22 @@ import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-do
 
 import Users from './users/pages/Users'
 import NewWorkout from './workouts/pages/NewWorkout'
+import MainNavigation from './shared/components/Navigation/MainNavigation'
 
   const App = () => {
   return  <Router>
-    <Switch>
-    <Route path="/"exact>
-      <Users />
-    </Route>
-    <Route path="/workouts/new"exact>
-      <NewWorkout />
-    </Route>
-    <Redirect  to="/" />
-    </Switch>
+    <MainNavigation />
+     <main> 
+      <Switch>
+       <Route path="/"exact>
+          <Users />
+        </Route>
+     <Route path="/workouts/new"exact>
+          <NewWorkout />
+        </Route>
+        <Redirect  to="/" />
+      </Switch>
+    </main>
   </Router>
   
 }
