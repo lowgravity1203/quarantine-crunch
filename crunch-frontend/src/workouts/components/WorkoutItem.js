c14f9fd8dc7c3ed9542687b5210e3546ca9bdacc
lowgravity1203 <matthew.f.barnes1203@gmail.com>
2020-08-26 16:06:01 -0700

    adds modal and delete functionality

--- crunch-frontend/src/workouts/components/WorkoutItem.js
+++ crunch-frontend/src/workouts/components/WorkoutItem.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 
+import Modal from '../../shared/components/UIElements/Modal'
 import Card from '../../shared/components/UIElements/Card'
 import Button from '../../shared/components/FormElements/Button'
 import './WorkoutItem.css'
 
 const WorkoutItem = props => {
+    const [showConfirmModal, setShowConfirmModal] = useState(false)
+
+    const showDeleteWarningHandler = () => {
+        setShowConfirmModal(true)
+    }
+
+    const cancelDeleteHandler = () => {
+        setShowConfirmModal(false)
+    }
+
+    const confirmDeleteHandler = () => {
+        setShowConfirmModal(false)
+        console.log('DELETING...')
+    }
+
     return (
+        <React.Fragment>
+            <Modal 
+            show={showConfirmModal}
+            onCancel={cancelDeleteHandler}
+            header="Are you sure?" 
+            footerClass='place-item__modal-actions' 
+            footer={
+                <React.Fragment>
+                    <Button inverse onClick={cancelDeleteHandler} >Cancel</Button>
+                    <Button danger onClick={confirmDeleteHandler} >Delete</Button>
+                </React.Fragment>
+            } >
+                <p>Do you want to proceed and delete this workout? Please note that it can't be undone.</p>
+            </Modal>
      <li className="place-item">
          <Card className="place-item__content"> 
          <div className="place-item__image">
@@ -17,10 +47,11 @@ const WorkoutItem = props => {
          </div>
          <div className="place-item__actions">
              <Button to={`/places/${props.id}`}>Edit</Button>
-             <Button danger>Delete</Button>
+             <Button danger onClick={showDeleteWarningHandler} >Delete</Button>
          </div>
          </Card>
      </li>
+     </React.Fragment>
     );
 };
 
